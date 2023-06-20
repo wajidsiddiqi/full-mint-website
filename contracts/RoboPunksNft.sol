e54170f5a052d58f02813dd896faa06796b17185
Wajid <wajidsiddiqi12@gmail.com>
2023-06-20 22:40:58 +0500

    changes in contract

--- contracts/RoboPunksNft.sol
+++ contracts/RoboPunksNft.sol
@@ -8,13 +8,13 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract RoboPunksNFT is ERC721, Ownable {
-    uint256 private immutable i_mintPrice;
-    uint256 private immutable i_totalSupply;
-    uint256 private immutable i_maxSupply;
+    uint256 internal immutable i_mintPrice;
+    uint256 private s_totalSupply;
+    uint256 private s_maxSupply;
     uint256 private immutable i_maxWalletLimit;
-    bool private nftMintState;
-    string private s_baseTokenURI;
-    mapping(address => uint) private s_walletMints;
+    bool private s_nftMintState;
+    string[] internal s_baseTokenURI;
+    mapping(address => uint256) private s_walletMints;
 
     constructor(
         uint256 _mintPrice,
@@ -22,13 +22,42 @@ contract RoboPunksNFT is ERC721, Ownable {
         uint256 _maxSupply,
         uint256 _maxWalletLimit,
         bool _nftMintState,
-        string _baseTokenURI
+        string[] memory _baseTokenURI
     ) ERC721("RoboPunks", "RP") {
-        _mintPrice = i_mintPrice;
-        _totalSupply = i_totalSupply;
-        _maxSupply = i_maxSupply;
-        _maxWalletLimit = i_maxWalletLimit;
-        _nftMintState = nftMintState;
-        _baseTokenURI = s_baseTokenURI;
+        i_mintPrice = _mintPrice;
+        s_totalSupply = _totalSupply;
+        s_maxSupply = _maxSupply;
+        i_maxWalletLimit = _maxWalletLimit;
+        s_nftMintState = _nftMintState;
+        s_baseTokenURI = _baseTokenURI;
+    }
+
+    function changeNftMintState(bool state) private onlyOwner {
+        s_nftMintState = state;
+    }
+
+    //*Getter Functions
+    function getMintPrice() public view returns (uint256) {
+        return i_mintPrice;
+    }
+
+    function getTotalSupply() public view returns (uint256) {
+        return s_totalSupply;
+    }
+
+    function getMaxSupply() public view returns (uint256) {
+        return s_maxSupply;
+    }
+
+    function getWalletLimit() public view returns (uint256) {
+        return i_maxWalletLimit;
+    }
+
+    function getNftState() public view returns (bool) {
+        return s_nftMintState;
+    }
+
+    function getNftState(address) public view returns (uint256) {
+        return s_walletMints[address];
     }
 }
