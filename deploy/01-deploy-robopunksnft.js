13f03382d69deddeca399835d8da134870313a9b
Wajid <wajidsiddiqi12@gmail.com>
2023-06-30 22:52:59 +0500

    updated deploy

--- deploy/01-deploy-robopunksnft.js
+++ deploy/01-deploy-robopunksnft.js
@@ -5,54 +5,14 @@ const {
 } = require("../helper-hardhat-config");
 const { verify } = require("../utils/verify");
 
-/*
-const imagesLocation = "./images/randomNft";
-
-const metadataTemplate = {
-  name: "",
-  description: "",
-  image: "",
-  attributes: [
-    {
-      trait_type: "cuteness",
-      value: 100,
-    },
-  ],
-};
-*/
-
-// const VRF_SUB_FUND_AMMOUNT = ethers.utils.parseEther("2");
-
-/*const tokenUris = [
-  "ipfs://QmQs4yASJakykKzcUYiJoQEFptCuufghNA3S5J2CkD47tp",
-  "ipfs://QmXry9jwWVKfbt6V87Gzd97WJ5LGAmtyWY7znSQXCRysv9",
-  "ipfs://QmX5V7Xc31vMfM8tYgrNefix1WCFmiMqpLzjDtk6PgTQd2",
-];*/
-
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
   const chainId = network.config.chainId;
 
-  const mintFee = ethers.utils.parseEther("0.4");
-  const totalSupply = 0;
-  const maxSupply = 8;
-  const walletLimit = 2;
-  const nftState = false;
-  const tokenURI = [];
-
-  const args = [
-    mintFee,
-    totalSupply,
-    maxSupply,
-    walletLimit,
-    nftState,
-    tokenURI,
-  ];
-
   const roboPunksNft = await deploy("RoboPunksNFT", {
     from: deployer,
-    args: args,
+    args: [],
     log: true,
     waitConfirmations: network.config.blockConfirmations || 1,
   });
@@ -67,26 +27,5 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
   log("---------------------------------------");
 };
-
-/*const handleTokenUris = async () => {
-  tokenUris = [];
-  const { responses: imageUploadResponses, files } = await storeImages(
-    imagesLocation
-  );
-  for (let imageUploadResponseIndex in imageUploadResponses) {
-    let tokenUriMetadata = { ...metadataTemplate };
-    tokenUriMetadata.name = files[imageUploadResponseIndex].replace(".png", "");
-    tokenUriMetadata.description = `An adorable ${tokenUriMetadata.name} pup!`;
-    tokenUriMetadata.image = `ipfs://${imageUploadResponses[imageUploadResponseIndex].IpfsHash}`;
-    console.log(`Uploading ${tokenUriMetadata.name}...`);
-    const metadataUploadResponse = await storeTokenUriMetadata(
-      tokenUriMetadata
-    );
-    tokenUris.push(`ipfs://${metadataUploadResponse.IpfsHash}`);
-  }
-  console.log("Token URIs uploaded! They are:");
-  console.log(tokenUris);
-  return tokenUris;
-};*/
 
 module.exports.tags = ["all", "roboPunksNft", "main"];
