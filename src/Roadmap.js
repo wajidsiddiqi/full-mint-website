5230bf7a1c1ed8a002db91d36322fcb5ecec0056
Wajid <wajidsiddiqi12@gmail.com>
2023-07-15 01:34:02 +0500

    completed roadmap js

--- src/Roadmap.js
+++ src/Roadmap.js
@@ -1,37 +1,67 @@
 import { Box, Flex, Text } from "@chakra-ui/react";
+import Twitter from "./assets/social-media-icons/twitter.png";
 
 const Roadmap = () => {
   return (
     <Flex
-      justify="center"
-      align="center"
-      height="100vh"
+      flexDirection="column"
+      minHeight="100vh"
       maxWidth="1600px"
       margin="auto"
     >
-      <Box width="520px">
-        <div>
-          <Text fontSize="40px" textShadow="0 5px #000000">
-            Roadmap
+      <Flex
+        justify="center"
+        align="center"
+        flexGrow="1"
+        padding="90px 2px 30px 2px"
+      >
+        <Box width="520px">
+          <div>
+            <Text fontSize="40px" textShadow="0 5px #000000">
+              Roadmap
+            </Text>
+            <Text
+              fontSize="30px"
+              letterSpacing="-5.5%"
+              fontFamily="VT323"
+              textShadow="0 2px 2px #000000"
+              marginTop="4rem"
+              marginBottom="4rem"
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+              <br />
+              <br />
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id est laborum.
+              <br />
+              <br />A diam maecenas sed enim ut sem viverra aliquet eget.
+              Aliquam nulla facilisi cras fermentum odio eu feugiat pretium.
+              Pretium nibh ipsum consequat nisl vel.
+            </Text>
+          </div>
+        </Box>
+      </Flex>
+      <Box as="footer" py="4" textAlign="center">
+        <Flex justifyContent="center" alignItems="center">
+          <Text fontSize="25px" opacity="0.8" fontFamily="VT323">
+            Developed with love by Wajid © 2023 |{" "}
           </Text>
-          <Text
-            fontSize="30px"
-            letterSpacing="-5.5%"
-            fontFamily="VT323"
-            textShadow="0 2px 2px #000000"
-          >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.{"\n"}
-            Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.{"\n"}A diam maecenas sed enim ut sem viverra
-            aliquet eget. Aliquam nulla facilisi cras fermentum odio eu feugiat
-            pretium. Pretium nibh ipsum consequat nisl vel.
-          </Text>
-        </div>
+          <a href="#">
+            <img
+              src={Twitter}
+              alt="Twitter"
+              width="25px"
+              height="auto"
+              cursor="pointer"
+              margin-left="10px"
+            />
+          </a>
+        </Flex>
       </Box>
     </Flex>
   );
