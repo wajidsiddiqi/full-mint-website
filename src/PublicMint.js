f81763ecb04648c0277c14a421a01f81193f6b47
Wajid <wajidsiddiqi12@gmail.com>
2023-07-15 02:20:02 +0500

    added flex in pm

--- src/PublicMint.js
+++ src/PublicMint.js
@@ -55,135 +55,136 @@ const PublicMint = () => {
 
   return (
     <Flex
-      justify="center"
-      align="center"
-      height="100vh"
-      paddingBottom="120px"
+      flexDirection="column"
+      minHeight="100vh"
       maxWidth="1600px"
       margin="auto"
     >
-      <Box width="520px">
-        <div>
-          <Text fontSize="40px" textShadow="0 5px #000000">
-            RoboPunks
-          </Text>
-          <Text
-            fontSize="30px"
-            letterSpacing="-5.5%"
-            fontFamily="VT323"
-            textShadow="0 2px 2px #000000"
-          >
-            This is my full-stack NFT portfolio development project, created
-            using Solidity, Hardhat, React, WAGMI, Chakra, and various other
-            technologies. The project implements the ERC-721 token standard.
-          </Text>
-        </div>
-
-        {isConnected ? (
+      <Flex justify="center" align="center" flexGrow="1" padding="90px 2%">
+        <Box width="520px">
           <div>
-            <Flex align="center" justify="center">
-              <Button
-                backgroundColor="#D6517D"
-                borderRadius="5px"
-                boxShadow="0px 2px 2px 1px #0F0F0F"
-                color="white"
-                cursor="pointer"
-                fontFamily="inherit"
-                padding="15px"
-                marginTop="10px"
-                onClick={handleDecrement}
-              >
-                -
-              </Button>
-              <Input
-                type="number"
-                readOnly
-                fontFamily="inherit"
-                width="100px"
-                height="40px"
-                textAlign="center"
-                paddingLeft="19px"
-                marginTop="10px"
-                value={mintQuantity}
-              ></Input>
-              <Button
-                backgroundColor="#D6517D"
-                borderRadius="5px"
-                boxShadow="0px 2px 2px 1px #0F0F0F"
-                color="white"
-                cursor="pointer"
-                fontFamily="inherit"
-                padding="15px"
-                marginTop="10px"
-                onClick={handleIncrement}
-              >
-                +
-              </Button>
-            </Flex>
-            <Button
-              backgroundColor="#D6517D"
-              borderRadius="5px"
-              boxShadow="0px 2px 2px 1px #0F0F0F"
-              color="white"
-              cursor="pointer"
-              fontFamily="inherit"
-              padding="15px"
-              marginTop="10px"
-              disabled={!write || isLoading}
-              onClick={write}
+            <Text fontSize="40px" textShadow="0 5px #000000">
+              RoboPunks
+            </Text>
+            <Text
+              fontSize="30px"
+              letterSpacing="-5.5%"
+              fontFamily="VT323"
+              textShadow="0 2px 2px #000000"
             >
-              {isLoading ? "MINTING..." : "MINT NOW!"}
-            </Button>
-            {isSuccess && (
-              <Flex align="center" justify="center">
-                <Text
-                  fontSize="30px"
-                  fontFamily="VT323"
-                  textShadow="0 3px #000000"
-                  color="#2bd42b"
-                >
-                  Successfully minted your NFT!
-                  <Spacer />
-                  <a
-                    href={`https://sepolia.etherscan.io/tx/${data?.hash}`}
-                    style={{
-                      color: "#D6517D",
-                      textDecoration: "none",
-                    }}
-                  >
-                    Etherscan
-                  </a>
-                </Text>
-              </Flex>
-            )}
-            {(isPrepareError || isError) && (
-              <Flex align="center" justify="center">
-                <Text
-                  fontSize="25px"
-                  fontFamily="VT323"
-                  textShadow="0 3px #000000"
-                  marginTop="35px"
-                  color="red"
-                >
-                  Error:{" "}
-                  {extractErrorReason(prepareError?.message) || error?.message}
-                </Text>
-              </Flex>
-            )}
+              This is my full-stack NFT portfolio development project, created
+              using Solidity, Hardhat, React, WAGMI, Chakra, and various other
+              technologies. The project implements the ERC-721 token standard.
+            </Text>
           </div>
-        ) : (
-          <Text
-            fontSize="30px"
-            letterSpacing="-5.5%"
-            fontFamily="VT323"
-            textShadow="0 3px #000000"
-            marginTop="70px"
-            color="#D6517D"
-          >
-            You must be connected to Mint!
-          </Text>
-        )}
-      </Box>
+
+          {isConnected ? (
+            <div>
+              <Flex align="center" justify="center">
+                <Button
+                  backgroundColor="#D6517D"
+                  borderRadius="5px"
+                  boxShadow="0px 2px 2px 1px #0F0F0F"
+                  color="white"
+                  cursor="pointer"
+                  fontFamily="inherit"
+                  padding="15px"
+                  marginTop="10px"
+                  onClick={handleDecrement}
+                >
+                  -
+                </Button>
+                <Input
+                  type="number"
+                  readOnly
+                  fontFamily="inherit"
+                  width="100px"
+                  height="40px"
+                  textAlign="center"
+                  paddingLeft="19px"
+                  marginTop="10px"
+                  value={mintQuantity}
+                ></Input>
+                <Button
+                  backgroundColor="#D6517D"
+                  borderRadius="5px"
+                  boxShadow="0px 2px 2px 1px #0F0F0F"
+                  color="white"
+                  cursor="pointer"
+                  fontFamily="inherit"
+                  padding="15px"
+                  marginTop="10px"
+                  onClick={handleIncrement}
+                >
+                  +
+                </Button>
+              </Flex>
+              <Button
+                backgroundColor="#D6517D"
+                borderRadius="5px"
+                boxShadow="0px 2px 2px 1px #0F0F0F"
+                color="white"
+                cursor="pointer"
+                fontFamily="inherit"
+                padding="15px"
+                marginTop="10px"
+                disabled={!write || isLoading}
+                onClick={write}
+              >
+                {isLoading ? "MINTING..." : "MINT NOW!"}
+              </Button>
+              {isSuccess && (
+                <Flex align="center" justify="center">
+                  <Text
+                    fontSize="30px"
+                    fontFamily="VT323"
+                    textShadow="0 3px #000000"
+                    color="#2bd42b"
+                  >
+                    Successfully minted your NFT!
+                    <Spacer />
+                    <a
+                      href={`https://sepolia.etherscan.io/tx/${data?.hash}`}
+                      style={{
+                        color: "#D6517D",
+                        textDecoration: "none",
+                      }}
+                    >
+                      Etherscan
+                    </a>
+                  </Text>
+                </Flex>
+              )}
+              {(isPrepareError || isError) && (
+                <Flex align="center" justify="center">
+                  <Text
+                    fontSize="25px"
+                    fontFamily="VT323"
+                    textShadow="0 3px #000000"
+                    marginTop="35px"
+                    color="red"
+                  >
+                    Error:{" "}
+                    {extractErrorReason(prepareError?.message) ||
+                      error?.message}
+                  </Text>
+                </Flex>
+              )}
+            </div>
+          ) : (
+            <Text
+              fontSize="30px"
+              letterSpacing="-5.5%"
+              fontFamily="VT323"
+              textShadow="0 3px #000000"
+              marginTop="70px"
+              color="#D6517D"
+            >
+              You must be connected to Mint!
+            </Text>
+          )}
+        </Box>
+      </Flex>
     </Flex>
   );
 };
