0aa43791218de31e3a8a12f6ddf387c25de0b0fd
Wajid <wajidsiddiqi12@gmail.com>
2023-07-17 23:03:16 +0500

    added scroll to top

---
+++ src/ScrollToTopButton.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import arrow from "./assets/social-media-icons/arrow.png";
+
+const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    setIsVisible(scrollTop > 100);
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <button
+      className={`scroll-to-top ${isVisible ? "show" : ""}`}
+      onClick={scrollToTop}
+    >
+      <img src={arrow} alt="Scroll to Top" />
+    </button>
+  );
+};
+
+export default ScrollToTopButton;
